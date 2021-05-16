c46ec7d7a135e30abbcd86b8872eceb885d89d3e
EgorKorobkin <glebkorobkin@bk.ru>
2021-05-16 21:05:51 +0500

    added section middle

---
+++ app/js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+    var mixer = mixitup('.newest__all-products');
+
+    
+});
+
+		
+$('.trending__inner-products').slick({
+    arrows: false,
+    dots:true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });	
+
